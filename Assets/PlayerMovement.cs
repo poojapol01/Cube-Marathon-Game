1c85b434ba1950c35eebba49ea3fb49fdca5f185
poojapol01 <polpooja05@gmail.com>
2023-04-25 23:34:53 +0530

    Updated Player Movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,10 +6,21 @@ public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     public Rigidbody rb;
+    public float forwardForce = 1000f;
+    public float sidewaysForce = 600;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 400 * Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); // Add a force of 2000 on Z-Axis
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(- sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
